e70fbb40ee89194283637c071a3debc9a56e4788
Alan Poon <alan.poon@shopee.com>
2021-11-23 10:37:49 +0800

    Merge branch 'crates' into 0.5

--- actors/web_server/build.rs
+++
@@ -1,5 +0,0 @@
-use actix_web_static_files::resource_dir;
-
-fn main() {
-    resource_dir("./../../ui/build").build().unwrap();
-}
--- actors/web_server/src/actix_web_static_files.rs
+++
@@ -1,5 +0,0 @@
-pub struct Resource {
-  pub data: &'static [u8],
-  pub modified: u64,
-  pub mime_type: &'static str,
-}
