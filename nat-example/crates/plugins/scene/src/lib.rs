44f91d0a9f3df91fd8415448948f803f7a7de567
Alan Poon <brightshadowx@gmail.com>
2022-01-17 12:36:15 +0800

    scene

--- nat-example/crates/plugins/scene/src/lib.rs
+++ nat-example/crates/plugins/scene/src/lib.rs
@@ -8,6 +8,7 @@ use std::f32::consts::PI;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 use rand::Rng;
+use bevy_obj::*;
 
 #[wasm_bindgen]
 extern "C" {
@@ -38,7 +39,9 @@ impl Plugin for QQScenePlugin {
         app.register_type::<ComponentA>()
         .register_type::<ComponentB>()
         .add_startup_system(save_scene_system.exclusive_system())
-        .add_startup_system(load_scene_system);
+        .add_startup_system(load_scene_system)
+        .add_plugin(ObjPlugin)
+        .add_startup_system(setup.system());
     }
 }
 #[derive(Component, Reflect, Default)]
@@ -101,4 +104,27 @@ fn save_scene_system(world: &mut World) {
     info!("{}", scene.serialize_ron(type_registry).unwrap());
 
     // TODO: save scene
+}
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn_bundle(PbrBundle {
+        mesh: asset_server.load("model/Walk1.obj"),
+        material: materials.add(StandardMaterial {
+            base_color_texture: Some(asset_server.load("textures/DrumstickTexture.png")),
+            ..Default::default()
+        }),
+        ..Default::default()
+    });
+    commands.spawn_bundle(PointLightBundle {
+        transform: Transform::from_translation(Vec3::new(3.0, 4.0, 3.0)),
+        ..Default::default()
+    });
+    commands.spawn_bundle(PerspectiveCameraBundle {
+        transform: Transform::from_translation(Vec3::new(1.5, 2.7, 4.0))
+            .looking_at(Vec3::new(0.0, 0.0, 0.0), Vec3::Y),
+        ..Default::default()
+    });
 }
